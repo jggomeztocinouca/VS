dc235e7b3f0ea06a4d0fcb5cac940585bc6480c2
Jesús Gómez <90068817+jggomeztocinouca@users.noreply.github.com>
2023-11-25 13:07:09 +0100

    Actualizado el script de despliegue

--- Entregable Kubernetes/Cluster/deploy.ps1
+++ Entregable Kubernetes/Cluster/deploy.ps1
@@ -1,14 +1,20 @@
 # Crear el clúster con Kind
+Write-Host "Creando el clúster con Kind..."
 kind create cluster --config kind-config.yaml
 
 # Despliegue de los volumenes persistentes
+Write-Host "`nDesplegando los volúmenes persistentes para Drupal..."
 kubectl apply -f drupal-persistent-volumes.yaml
+
+Write-Host "`nDesplegando los volúmenes persistentes para MySQL..."
 kubectl apply -f mysql-persistent-volumes.yaml
 
 # Despligue de los servicios y aplicaciones de MySQL
+Write-Host "`nDesplegando los servicios y aplicaciones de MySQL..."
 kubectl apply -f mysql-deployment.yaml
 kubectl apply -f mysql-sv.yaml
 
-# Despligue de los servicios y aplicaciones de MySQL
+# Despligue de los servicios y aplicaciones de Drupal
+Write-Host "`nDesplegando los servicios y aplicaciones de Drupal..."
 kubectl apply -f drupal-deployment.yaml
 kubectl apply -f drupal-sv.yaml
