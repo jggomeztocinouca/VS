f9c65d5e474c2bad5ef213875e4bf5a37c8f2056
Jesús Gómez <90068817+jggomeztocinouca@users.noreply.github.com>
2023-12-16 19:41:33 +0100

    redistribution of files

--- TeamCity/pipeline.kts
+++ TeamCity/pipeline.kts
@@ -14,17 +14,17 @@ project {
             }
             dockerWrapper {
                 name = "Test de dependencias"
-                command = "./jenkins/scripts/test.sh"
+                command = "./scripts/test.sh"
                 containerName = "VS"
             }
             dockerWrapper {
                 name = "Detener servicio"
-                command = "./jenkins/scripts/stop.sh"
+                command = "./scripts/stop.sh"
                 containerName = "VS"
             }
             dockerWrapper {
                 name = "Eliminar contenedor"
-                command = "./jenkins/scripts/delete.sh"
+                command = "./scripts/delete.sh"
                 containerName = "VS"
             }
         }
