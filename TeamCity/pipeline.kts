eb3ef9b0bc86eeb53856d23406195e0e59f19803
Jesús Gómez <90068817+jggomeztocinouca@users.noreply.github.com>
2023-12-16 20:36:55 +0100

    modified kts

--- TeamCity/pipeline.kts
+++ TeamCity/pipeline.kts
@@ -2,7 +2,7 @@ import jetbrains.buildServer.configs.kotlin.v2019_2.*
 import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.dockerCommand
 import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.script
 
-version = "2021.1"
+version = "2023.11"
 
 project {
     buildType {
@@ -10,23 +10,25 @@ project {
 
         steps {
             dockerCommand {
-                name = "Crear y ejecutar contenedor 'VS'"
-                commandType = run {
-                    image = "node:21-alpine3.18"
-                    args = "--name VS"
+                name = "Crear contenedor"
+                commandType = customScript {
+                    scriptContent = "docker run -d --name VS node:21-alpine3.18"
                 }
             }
             script {
                 name = "Test de dependencias"
                 scriptContent = "./scripts/test.sh"
+                executionMode = BuildStep.ExecutionMode.RUN_ON_SUCCESS
             }
             script {
                 name = "Detener servicio"
                 scriptContent = "./scripts/stop.sh"
+                executionMode = BuildStep.ExecutionMode.RUN_ON_SUCCESS
             }
             script {
                 name = "Eliminar contenedor"
                 scriptContent = "./scripts/delete.sh"
+                executionMode = BuildStep.ExecutionMode.RUN_ON_SUCCESS
             }
         }
     }
